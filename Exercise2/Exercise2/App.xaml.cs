3970fcd8f7c9dee18d2c6d1c1f7bb4297a257850
Hero Pineda <heropineda@yahoo.com.ph>
2017-12-12 10:30:29 +0800

    Display alert button

---
+++ Exercise2/Exercise2/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace Exercise2
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("70a946da-c969-4a06-8345-fad9517dd3f6",
+                    typeof(Analytics), typeof(Crashes));
+
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Exercise2/Exercise2/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exercise2
+{
+	public partial class MainPage : ContentPage
+	{
+
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        //        void OnButtonClicked(object sender, EventArgs args)
+        //        {
+        //            var result = await this.DisplayAlert("Notification", "Do you want store this Number ?", "Yes", "No");
+
+        ////            Navigation.PushAsync(new AboutUs());
+        //        }
+
+        //protected override bool OnBackButtonPressed()
+        //{
+        //    Device.BeginInvokeOnMainThread(async () => {
+        //        var result = await this.DisplayAlert("Alert!", "Do you really want to exit?", "Yes", "No");
+        //        if (result) await this.Navigation.PopAsync(); // or anything else
+        //    });
+
+        //    return true;
+        //}
+
+        private async void Button_Onclick(object sender, EventArgs e)
+        {
+            //Device.BeginInvokeOnMainThread(async () => {
+            //    var result = await this.DisplayAlert("Alert!", "Do you really want to continue?", "Yes", "No");
+            //    if (result) await Navigation.PushAsync(new Sample()); // or anything else
+            //});
+            //return true;
+
+            var result = await DisplayAlert("title", "Hello", "OK", "Cancel");
+            if (result.Equals(true))
+            {
+            Navigation.PushAsync(new AboutUs());
+            }
+        }
+
+    }
+}
